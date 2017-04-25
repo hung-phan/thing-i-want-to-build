1dbbcc4f6082840cb32ede09407ef81ff4ff8f62
hung-phan <colorvisavn@gmail.com>
2017-04-25 15:09:47 +0700

    Update url

--- tinyurl/application/static/js/main.js
+++ tinyurl/application/static/js/main.js
@@ -5,8 +5,11 @@
     var originalURL = form.find("#originalURL").val();
 
     $.post("/shorten_url", { original_url: originalURL })
-      .done(function() {
-        console.log(arguments);
+      .done(function(result, status) {
+        if (status === "success") {
+          var tinyurl = window.location.protocol + "//" + window.location.host + "/urls/" + result.id;
+          $("#result").html(tinyurl);
+        }
       });
   });
 })(window.jQuery);
