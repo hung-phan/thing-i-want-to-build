9facb7cc55ca8dfaa51ac984a6a313074892336a
hung-phan <colorvisavn@gmail.com>
2017-04-28 17:07:11 +0700

    Working version

--- pubsub/src/server_1.py
+++ pubsub/src/server_1.py
@@ -3,4 +3,4 @@ from kafka import KafkaProducer
 producer = KafkaProducer()
 
 for _ in range(100):
-    producer.send("foobar", "some_message_bytes")
+    producer.send("foobar", b"some_message_bytes")
